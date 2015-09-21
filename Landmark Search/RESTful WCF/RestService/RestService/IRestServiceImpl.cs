d478270c315439b68c22d6ef0a79964629dc611e
NGUYEN Ngoc Chau Sang <ngoc-chau-sang.nguyen@eurecom.fr>
2015-09-21 13:10:18 +0200

    Update code and documents

---
+++ Landmark Search/RESTful WCF/RestService/RestService/IRestServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.IO;
+
+namespace RestService
+{
+    
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        //[OperationContract]
+        //[WebInvoke(Method = "POST", UriTemplate = "upImage/{id}")]
+        //string UploadImage(string id, Stream fileStream);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "hello/")]
+        string Hello();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "init/")]
+        string Init();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "detect/{name}")]
+        string Detect(string name);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "matching/{name}")]
+        string Matching(string name);
+    }
+}
